323c0383496deaa117cc8c95caa9e1ea37889b2b
Nikhil Das <nikhil.thottupurathu@igate.com>
2015-11-06 12:24:18 +0530

    Added custom icon to main window. File > Exit implemented

--- Sanity_UI/src/MainWindow.java
+++ Sanity_UI/src/MainWindow.java
@@ -1,33 +1,40 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingConstants;
-import javax.swing.JMenu;
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 
-public class MainWindow extends JFrame {
-
+public class MainWindow extends JFrame 
+{
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
 					MainWindow frame = new MainWindow();
 					frame.setVisible(true);
-				} catch (Exception e) {
+				}
+				catch (Exception e) 
+				{
 					e.printStackTrace();
 				}
 			}
@@ -37,7 +44,22 @@ public class MainWindow extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MainWindow() {
+	public MainWindow() 
+	{
+		
+		File MainWindowIcon = new File("Resources/Icons/MainWindowIcon.png");
+		try 
+		{
+			setIconImage(ImageIO.read(MainWindowIcon ));
+		}
+		catch (IOException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//setIconImage(ImageIO.read("res/icon.png"));
+		
+		setTitle("Sanity Automation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
@@ -46,15 +68,18 @@ public class MainWindow extends JFrame {
 		
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+				
 		JMenuItem mntmExit = new JMenuItem("Exit");
-		mntmExit.addMouseListener(new MouseAdapter() {
+		mntmExit.addMouseListener(new MouseAdapter() 
+		{
+
 			@Override
-			public void mouseClicked(MouseEvent arg0) 
+			public void mouseReleased(MouseEvent e) 
 			{
-				//TODO Exit button Clicked
+				System.exit(0);
 			}
 		});
+		
 		mnFile.add(mntmExit);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
